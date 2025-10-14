5fe8d675026f815d114e02dad627b2bc017984d0
mewtwo <mewtwo020906@gmail.com>
2025-10-14 21:59:26 +0800

    方法在执行过程中内存的变化，方法的的重载以及方法的递归调用

--- F.java
+++ F.java
@@ -1,27 +1,13 @@
 public class F{
-    static int k;
     public static void main(String[] args){
-        
-        int count = 0;
-
-        for( int i = 2 ; i < 1001 ; i++){
-            boolean isPrime = true;
-            for( int j = 2 ; j < i ; j++){
-                if( i % j ==0 ){
-                    isPrime = false;
-                    break;
-                }
-            }
-                System.out.print( isPrime ? i + "是素数 ": "" );
-                if(isPrime){
-                    count++;
-                    if(count % 10 == 0){
-                        System.out.println();
-                }
-            }
-        
-
-        }
-        System.out.println("从1到1000中素数一共有" + count);
+        System.out.println(mulitiply(3));
     }
+    public static int mulitiply(int i){
+        if (i > 1) {
+            return i * mulitiply(i-1);
+        }else{
+            return 1;
+        }
+    }
+
 }
